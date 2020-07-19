59c417a8d81c0a17ccfc09aec3e6b225ba6e2b72
huixiongyu <huixiongyu@gmail.com>
2020-07-19 11:06:52 +0800

    block interview

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,10 +13,6 @@ module.exports = {
                 text: "博客",
                 link: "/blog/"
             },
-            {
-                text: "面试题",
-                link: "/interview/"
-            },
             { 
                 text: '阅读', 
                 link: "/book/"
@@ -54,24 +50,6 @@ module.exports = {
                         '/blog/Linux/typecho-migration'
                     ]
                 }
-            ],
-            '/interview/': [
-                '',
-                '00-experiences',
-                '01-html-css',
-                '02-javascript',
-                '03-vue',
-                '04-React',
-                '05-min-program',
-                '06-jquery',
-                '07-about-frontend',
-                '08-mobile',
-                '08-webpack',
-                '09-network-principle',
-                '10-code',
-                '11-algorithm',
-                '12-other',
-                '99-personal'
             ],
             '/book/': [
                 '',
---
+++ temp/config.js
@@ -0,0 +1,25 @@
+const nav = {
+    text: "面试题",
+    link: "/interview/"
+}
+
+const sidebar = {
+    '/interview/': [
+        '',
+        '00-experiences',
+        '01-html-css',
+        '02-javascript',
+        '03-vue',
+        '04-React',
+        '05-min-program',
+        '06-jquery',
+        '07-about-frontend',
+        '08-mobile',
+        '08-webpack',
+        '09-network-principle',
+        '10-code',
+        '11-algorithm',
+        '12-other',
+        '99-personal'
+    ],
+}
